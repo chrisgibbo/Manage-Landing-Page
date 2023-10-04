f55039f9dde5c523f5ea73dba34e2b7f0852e222
Chris Gibbons <christ487@icloud.com>
2023-10-04 20:24:29 +0100

    started styling with tailwind

--- manage-landing-page/src/components/heroNavSection.js
+++ manage-landing-page/src/components/heroNavSection.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function heroNavSection() {
   return (
-    <div className="px-28 h-screen">
+    <div className="px-28 h-screen box-border">
       {/* navigation Bar */}
       <nav className="flex justify-between items-center pt-8 pb-16">
         <span>Logo</span>
         <div>
-          <ul className="flex flex-row space-x-8">
+          <ul className="flex space-x-8">
             <li>Pricing</li>
             <li>Product</li>
             <li>About Us</li>
@@ -18,16 +18,18 @@ function heroNavSection() {
         <button>Get Started</button>
       </nav>
       {/* Hero Section */}
-      <div>
-        <div>
-          <h1>Bring everyone together to build better products</h1>
-          <p>
+      <div className="flex">
+        <div className="w-1/2 bg-red-200 pr-32">
+          <h1 className="text-5xl font-bold pb-10">
+            Bring everyone together to build better products
+          </h1>
+          <p className="pb-10 text-sm pr-32">
             Manage makes it simple for software teams to plan day-to-day tasks
             while keeping the larger team goals in view.
           </p>
           <button>Get Started</button>
         </div>
-        <div>
+        <div className="flex justify-between items-center w-1/2 bg-blue-200">
           <img scr="*" alt="field of shit"></img>
           <img scr="*" alt="field of shit"></img>
           <img scr="*" alt="field of shit"></img>
