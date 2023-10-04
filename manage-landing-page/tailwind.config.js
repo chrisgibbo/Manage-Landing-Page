82f95d6d74b730dbf58f27e995f1d6eec302311f
Chris Gibbons <christ487@icloud.com>
2023-10-04 20:57:20 +0100

    include font family to tailwind config

--- manage-landing-page/tailwind.config.js
+++ manage-landing-page/tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        BeVietnamPro: ["Be Vietnam Pro", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
