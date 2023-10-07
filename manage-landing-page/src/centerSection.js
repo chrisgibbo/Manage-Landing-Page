284ad0afa274e7ff51a7ca66a8734f1be39608e0
Chris Gibbons <christ487@icloud.com>
2023-10-07 16:35:46 +0100

    remove duplicate code and replace with component

--- manage-landing-page/src/centerSection.js
+++ manage-landing-page/src/centerSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import InfoSection from "./components/infoSection";
 
 function centerSection() {
   return (
@@ -12,33 +13,27 @@ function centerSection() {
         </p>
       </div>
       <div>
-        <div>
-          <button>01</button>
-          <h3>Track company-wide progress</h3>
-          <p>
-            See how your day-to-day tasks fit into the wider vision. Go from
-            tracking progress at the milestone level all the way done to the
-            smallest of detail. Never lose sight of the bigger picture again.
-          </p>
-        </div>
-        <div>
-          <button>02</button>
-          <h3>Track company-wide progress</h3>
-          <p>
-            See how your day-to-day tasks fit into the wider vision. Go from
-            tracking progress at the milestone level all the way done to the
-            smallest of detail. Never lose sight of the bigger picture again.
-          </p>
-        </div>
-        <div>
-          <button>03</button>
-          <h3>Track company-wide progress</h3>
-          <p>
-            See how your day-to-day tasks fit into the wider vision. Go from
-            tracking progress at the milestone level all the way done to the
-            smallest of detail. Never lose sight of the bigger picture again.
-          </p>
-        </div>
+        <InfoSection
+          button="01"
+          title="Track company-wide progress"
+          paragraph="See how your day-to-day tasks fit into the wider vision. Go from
+        tracking progress at the milestone level all the way done to the
+        smallest of detail. Never lose sight of the bigger picture again."
+        />
+        <InfoSection
+          button="02"
+          title="Track company-wide progress"
+          paragraph="See how your day-to-day tasks fit into the wider vision. Go from
+        tracking progress at the milestone level all the way done to the
+        smallest of detail. Never lose sight of the bigger picture again."
+        />
+        <InfoSection
+          button="03"
+          title="Track company-wide progress"
+          paragraph="See how your day-to-day tasks fit into the wider vision. Go from
+        tracking progress at the milestone level all the way done to the
+        smallest of detail. Never lose sight of the bigger picture again."
+        />
       </div>
     </div>
   );
