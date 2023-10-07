7c9f5f9e1ab95c12ed4d250b85bd15f4dab563cb
Chris Gibbons <christ487@icloud.com>
2023-10-07 16:16:12 +0100

    Import and add new section to app.js

--- manage-landing-page/src/App.js
+++ manage-landing-page/src/App.js
@@ -1,10 +1,12 @@
 // import logo from "./logo.svg";
 import HeroNavSection from "./components/heroNavSection";
+import CenterSection from "./components/centerSection";
 
 function App() {
   return (
     <div>
       <HeroNavSection />
+      <CenterSection />
     </div>
   );
 }
