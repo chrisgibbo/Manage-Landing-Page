d1e08ea2dac93174c413a56d04b994ed9690dc1f
Chris Gibbons <christ487@icloud.com>
2023-10-07 16:38:35 +0100

    amend each prop with correct text

--- manage-landing-page/src/centerSection.js
+++ manage-landing-page/src/centerSection.js
@@ -23,16 +23,18 @@ function centerSection() {
         <InfoSection
           button="02"
           title="Track company-wide progress"
-          paragraph="See how your day-to-day tasks fit into the wider vision. Go from
-        tracking progress at the milestone level all the way done to the
-        smallest of detail. Never lose sight of the bigger picture again."
+          paragraph="Set internal delivery estimates and track progress toward
+          company goals. Our customisable dashboard helps you
+          build out the reports you need to keep key stakeholders
+          informed."
         />
         <InfoSection
           button="03"
           title="Track company-wide progress"
-          paragraph="See how your day-to-day tasks fit into the wider vision. Go from
-        tracking progress at the milestone level all the way done to the
-        smallest of detail. Never lose sight of the bigger picture again."
+          paragraph="Stop jumping from one service to another to 
+          communicate. Store files, track tasks and share
+          documents. Manage offers an all-in-one team 
+          productivity solution."
         />
       </div>
     </div>
