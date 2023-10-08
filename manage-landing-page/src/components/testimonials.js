906e7d0b2ad85ea7ad08b30c0340a4cb8d891745
Chris Gibbons <christ487@icloud.com>
2023-10-08 20:39:40 +0100

    adjust margin and padding, remove bg color red

--- manage-landing-page/src/components/testimonials.js
+++ manage-landing-page/src/components/testimonials.js
@@ -3,15 +3,15 @@ import "../App.css";
 
 function testimonials(props) {
   return (
-    <div className="flex flex-col items-center mx-2 mt-10 greyBackground relative h-44">
-      <div className="flex flex-col items-center h-24 bg-red-200">
+    <div className="flex flex-col items-center mx-2 mt-10 mb-6 greyBackground relative h-44">
+      <div className="flex flex-col items-center h-24 ">
         <img
           src={props.image}
           alt="Persons Face"
           className="h-1/3 absolute top-[-28px]"
         ></img>
       </div>
-      <div className="pb-10 px-6">
+      <div className="pb-7 px-6">
         <h3 className="darkBlue text-sm font-bold pb-6">{props.name}</h3>
         <p className="greyFont text-xs">{props.testimonial}</p>
       </div>
