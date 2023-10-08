afdcea8656cfea4206d7a3d75b53bd8772b8b4f5
Chris Gibbons <christ487@icloud.com>
2023-10-08 16:49:22 +0100

    add class greyBackground  to div

--- manage-landing-page/src/components/testimonials.js
+++ manage-landing-page/src/components/testimonials.js
@@ -3,7 +3,7 @@ import "../App.css";
 
 function testimonials(props) {
   return (
-    <div className="flex flex-col justify-center items-center ">
+    <div className="flex flex-col justify-center items-center greyBackground">
       <div>
         <img src={props.image} alt="Persons Face"></img>
       </div>
