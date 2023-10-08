96c60cef509c7a3a1a64888cb0f6f490115801fa
Chris Gibbons <christ487@icloud.com>
2023-10-08 16:46:05 +0100

    update text in testimonials

--- manage-landing-page/src/components/simpleSlider.js
+++ manage-landing-page/src/components/simpleSlider.js
@@ -28,23 +28,21 @@ function simpleSlider() {
       <Testimonials
         image={AliBravo}
         name="Ali Bravo"
-        testimonial="Manage has supercharged our team's workdflow. The ability 
-      to maintain visibility on larger milestones at all times
-      keeps everyone motivated."
+        testimonial="We have been able to cancel so many other subscriptions 
+        since using Manage. There is no more cross-channel confusion and everyone is much more focused."
       />
       <Testimonials
         image={RichardWatts}
         name="Richard Watts"
-        testimonial="Manage has supercharged our team's workdflow. The ability 
-      to maintain visibility on larger milestones at all times
-      keeps everyone motivated."
+        testimonial="Manage allows us to provide structure and process. It keeps 
+        us organized and focused. I can't stop recommending 
+        them to everyone I talk to!."
       />
       <Testimonials
         image={ShanaiGough}
         name="Shanai Gough"
-        testimonial="Manage has supercharged our team's workdflow. The ability 
-      to maintain visibility on larger milestones at all times
-      keeps everyone motivated."
+        testimonial="Their software allows us to track, manage and collaborate 
+        on our projects from anywhere. It keeps the whole team in-sync without being intrusive."
       />
     </Slider>
   );
