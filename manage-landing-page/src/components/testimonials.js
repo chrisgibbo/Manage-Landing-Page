d8acada5c85c69974738310abe2cc389b1f67526
Chris Gibbons <christ487@icloud.com>
2023-10-08 16:59:05 +0100

    center portraits on div

--- manage-landing-page/src/components/testimonials.js
+++ manage-landing-page/src/components/testimonials.js
@@ -3,13 +3,13 @@ import "../App.css";
 
 function testimonials(props) {
   return (
-    <div className="flex flex-col mx-3 mb-8 items-center greyBackground">
-      <div>
-        <img src={props.image} alt="Persons Face"></img>
+    <div className="flex flex-col items-center mx-3 mb-8 greyBackground">
+      <div className="flex flex-col items-center">
+        <img src={props.image} alt="Persons Face" className="w-1/2 "></img>
       </div>
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.testimonial}</p>
+        <h3 className="">{props.name}</h3>
+        <p className="">{props.testimonial}</p>
       </div>
     </div>
   );
