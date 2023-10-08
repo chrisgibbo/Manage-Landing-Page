fda3e1d73e1213e7d952d6c9330b744ad3dcfd7f
Chris Gibbons <christ487@icloud.com>
2023-10-08 16:39:36 +0100

    create new component with props for testimonials

---
+++ manage-landing-page/src/components/testimonials.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+function testimonials(props) {
+  return (
+    <div>
+      <div>
+        <img src={props.image} alt="Persons Face"></img>
+      </div>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.testimonial}</p>
+      </div>
+    </div>
+  );
+}
+
+export default testimonials;
