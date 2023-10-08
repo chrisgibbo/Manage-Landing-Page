8aba16cd7b5291890c2e0b16e69dd40a8da99d4c
Chris Gibbons <christ487@icloud.com>
2023-10-08 20:37:30 +0100

    adjust margin bottom on button

--- manage-landing-page/src/testimonialSection.js
+++ manage-landing-page/src/testimonialSection.js
@@ -6,7 +6,7 @@ import "./App.css";
 function testimonialSection() {
   return (
     <div className="text-center pt-12 h-screen box-border font-BeVietnamPro">
-      <div className="mb-14">
+      <div className="mb-16">
         <h1 className="text-4xl font-bold darkBlue pb-20">What they've said</h1>
         <SimpleSlider />
       </div>
