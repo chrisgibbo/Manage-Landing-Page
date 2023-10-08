0dba0b91f7a7e50fc290ce8975fc0cdf45f25afb
Chris Gibbons <christ487@icloud.com>
2023-10-08 15:21:41 +0100

    create testimonialSection folder, add syntax

---
+++ manage-landing-page/src/testimonialSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HeroButton from "./components/heroButton";
+import "./App.css";
+
+function testimonialSection() {
+  return (
+    <div className="px-28 pt-12 h-screen box-border font-BeVietnamPro">
+      <h1>What they've said</h1>
+      <div></div>
+      <HeroButton />
+    </div>
+  );
+}
+
+export default testimonialSection;
