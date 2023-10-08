4288313c135fe052675168d16e352c985baf660e
Chris Gibbons <christ487@icloud.com>
2023-10-08 20:54:51 +0100

    create new component, calltoaction

---
+++ manage-landing-page/src/components/callToAction.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.css";
+
+function callToAction() {
+  return (
+    <div>
+      <div>
+        <h2>Simplify how your team works today.</h2>
+      </div>
+      <button className="HeroBtnWhite py-3 px-7 text-xs rounded-full font-medium">
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default callToAction;
