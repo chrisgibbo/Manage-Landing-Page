9de0f86e5247472bed07322ccd35ef5d2bd7fc9c
Chris Gibbons <christ487@icloud.com>
2023-10-08 16:54:22 +0100

    add margin bottom to div by 14

--- manage-landing-page/src/testimonialSection.js
+++ manage-landing-page/src/testimonialSection.js
@@ -6,7 +6,7 @@ import "./App.css";
 function testimonialSection() {
   return (
     <div className="text-center pt-12 h-screen box-border font-BeVietnamPro">
-      <div className="">
+      <div className="mb-14">
         <h1 className="text-4xl font-bold darkBlue mb-8">What they've said</h1>
         <SimpleSlider />
       </div>
