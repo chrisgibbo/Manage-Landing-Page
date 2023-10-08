3bed920298b84a6a6289f6dc3e0a58d31231f127
Chris Gibbons <christ487@icloud.com>
2023-10-08 15:21:24 +0100

    import testimonialsection, add component

--- manage-landing-page/src/App.js
+++ manage-landing-page/src/App.js
@@ -1,12 +1,14 @@
 // import logo from "./logo.svg";
 import HeroNavSection from "./heroNavSection";
 import CenterSection from "./centerSection";
+import TestimonialSection from "./testimonialSection";
 
 function App() {
   return (
     <div>
       <HeroNavSection />
       <CenterSection />
+      <TestimonialSection />
     </div>
   );
 }
