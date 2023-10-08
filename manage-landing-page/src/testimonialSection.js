5b2225e885d5b3fd96a4523e5964249b23db844f
Chris Gibbons <christ487@icloud.com>
2023-10-08 15:53:03 +0100

    adjust visial placement within syntax

--- manage-landing-page/src/testimonialSection.js
+++ manage-landing-page/src/testimonialSection.js
@@ -6,9 +6,13 @@ import "./App.css";
 function testimonialSection() {
   return (
     <div className="text-center px-28 pt-12 h-screen box-border font-BeVietnamPro">
-      <h1 className="text-4xl font-bold darkBlue mb-8">What they've said</h1>
-      <SimpleSlider />
-      <HeroButton />
+      <div className="">
+        <h1 className="text-4xl font-bold darkBlue mb-8">What they've said</h1>
+        <SimpleSlider />
+      </div>
+      <div>
+        <HeroButton />
+      </div>
     </div>
   );
 }
