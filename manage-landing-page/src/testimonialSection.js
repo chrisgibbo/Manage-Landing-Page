69bbbcf61ae44e5f3771bac56e171de3c91b58d1
Chris Gibbons <christ487@icloud.com>
2023-10-08 15:45:23 +0100

    Import SimpleSlider from components

--- manage-landing-page/src/testimonialSection.js
+++ manage-landing-page/src/testimonialSection.js
@@ -1,12 +1,13 @@
 import React from "react";
 import HeroButton from "./components/heroButton";
+import SimpleSlider from "./components/simpleSlider";
 import "./App.css";
 
 function testimonialSection() {
   return (
     <div className="text-center px-28 pt-12 h-screen box-border font-BeVietnamPro">
       <h1 className="text-4xl font-bold darkBlue mb-8">What they've said</h1>
-      <div></div>
+      <SimpleSlider />
       <HeroButton />
     </div>
   );
