60e65a56dfd855867d981e7e7ee54cdba10ee244
Chris Gibbons <christ487@icloud.com>
2023-10-08 21:04:45 +0100

    add styling to calltoaction component

--- manage-landing-page/src/components/callToAction.js
+++ manage-landing-page/src/components/callToAction.js
@@ -3,11 +3,13 @@ import "../App.css";
 
 function callToAction() {
   return (
-    <div>
-      <div>
-        <h2>Simplify how your team works today.</h2>
+    <div className="px-28 flex justify-between items-center h-44 brightRedBkGround font-BeVietnamPro">
+      <div className="w-2/5">
+        <h2 className="text-white font-medium text-3xl ">
+          Simplify how your team works today.
+        </h2>
       </div>
-      <button className="HeroBtnWhite py-3 px-7 text-xs rounded-full font-medium">
+      <button className="HeroBtnWhite py-3 px-7 text-xs rounded-full font-semibold">
         Get Started
       </button>
     </div>
