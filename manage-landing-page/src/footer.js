98057f5fd53e7317411466ae7ef5f68b3447f239
Chris Gibbons <christ487@icloud.com>
2023-10-09 20:52:13 +0100

    create new component footer.js

---
+++ manage-landing-page/src/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function footer() {
+  return (
+    <div className="px-28 h-44 font-BeVietnamPro">
+      {/* Logo + social icons div */}
+    </div>
+  );
+}
+
+export default footer;
