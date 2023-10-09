f184d1488fbb855818e30e5ce27cf63ed7a8d894
Chris Gibbons <christ487@icloud.com>
2023-10-09 20:52:55 +0100

    add navlinks inside footer

--- manage-landing-page/src/footer.js
+++ manage-landing-page/src/footer.js
@@ -17,6 +17,40 @@ function footer() {
           <img scr="" alt=""></img>
         </div>
       </div>
+
+      {/* footer navlinks div */}
+
+      <div>
+        <div>
+          <ul>
+            <li>
+              <a href="*">Home</a>
+            </li>
+            <li>
+              <a href="*">Pricing</a>
+            </li>
+            <li>
+              <a href="*">Products</a>
+            </li>
+            <li>
+              <a href="*">About Us</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a href="*">Careers</a>
+            </li>
+            <li>
+              <a href="*">Community</a>
+            </li>
+            <li>
+              <a href="*">Privacy Policy</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
