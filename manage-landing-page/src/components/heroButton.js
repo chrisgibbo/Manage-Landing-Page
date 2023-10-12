a6c788b73929b6710e51343e43a013a3477f84ae
Chris Gibbons <christ487@icloud.com>
2023-10-12 18:43:00 +0100

    adjust opacity on hover

--- manage-landing-page/src/components/heroButton.js
+++ manage-landing-page/src/components/heroButton.js
@@ -3,7 +3,7 @@ import "../App.css";
 
 function HeroButton() {
   return (
-    <button className="HeroBtn py-3 px-7 text-xs rounded-full font-semibold font-BeVietnamPro hover:opacity-60">
+    <button className="HeroBtn py-3 px-7 text-xs rounded-full font-semibold font-BeVietnamPro hover:opacity-70">
       Get Started
     </button>
   );
