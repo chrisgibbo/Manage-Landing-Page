13723f57e6d8b1e15bddae2aba5060a4dc076aac
Chris Gibbons <christ487@icloud.com>
2023-10-12 18:38:23 +0100

    add hover to links in navbar

--- manage-landing-page/src/heroNavSection.js
+++ manage-landing-page/src/heroNavSection.js
@@ -13,19 +13,18 @@ function heroNavSection() {
           <img src={Logo} alt="MainLogo"></img>
         </span>
         <div>
-          <ul className="darkBlue flex text-xs font-medium space-x-8">
-            <li className="cursor-pointer">Pricing</li>
-            <li className="cursor-pointer">Product</li>
-            <li className="cursor-pointer">About Us</li>
-            <li className="cursor-pointer">Careers</li>
-            <li className="cursor-pointer">Community</li>
+          <ul className="darkBlue flex text-xs font-medium space-x-8 ">
+            <li className="cursor-pointer hover:text-slate-400">Pricing</li>
+            <li className="cursor-pointer hover:text-slate-400">Product</li>
+            <li className="cursor-pointer hover:text-slate-400">About Us</li>
+            <li className="cursor-pointer hover:text-slate-400">Careers</li>
+            <li className="cursor-pointer hover:text-slate-400">Community</li>
           </ul>
         </div>
         <HeroButton />
       </nav>
+      {/* Hero Section */}
       <div className="flex px-28 h-screen ">
-        {/* Hero Section */}
-
         <div className="flex justify-center ">
           <div className=" flex-col items-center  w-1/2 pt-20">
             <h1 className="text-5xl font-bold darkBlue pb-10">
