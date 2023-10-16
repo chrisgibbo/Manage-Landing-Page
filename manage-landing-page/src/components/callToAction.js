350b10ce78ed9e6e3f3e604d1497bebb8f121ea3
Chris Gibbons <christ487@icloud.com>
2023-10-16 20:32:17 +0100

    Increase size of button tailwindcss

--- manage-landing-page/src/components/callToAction.js
+++ manage-landing-page/src/components/callToAction.js
@@ -12,7 +12,7 @@ function callToAction() {
           Simplify how your team works today.
         </h2>
       </div>
-      <button className="HeroBtnWhite py-3 px-7 text-xs rounded-full font-semibold hover:opacity-70">
+      <button className="HeroBtnWhite py-3 px-7 text-xs rounded-full font-semibold hover:opacity-70 max-md:py-4 max-md:px-8">
         Get Started
       </button>
     </div>
