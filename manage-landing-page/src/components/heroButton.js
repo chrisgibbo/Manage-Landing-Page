366154d23f91387c032947b815daeee5a22a84b2
Chris Gibbons <christ487@icloud.com>
2023-10-16 20:32:56 +0100

    add mobile size to button, increase size

--- manage-landing-page/src/components/heroButton.js
+++ manage-landing-page/src/components/heroButton.js
@@ -3,7 +3,10 @@ import "../App.css";
 
 function HeroButton() {
   return (
-    <button className="HeroBtn py-3 px-7 text-xs rounded-full font-semibold font-BeVietnamPro hover:opacity-70">
+    <button
+      className="HeroBtn py-3 px-7 text-xs rounded-full font-semibold font-BeVietnamPro hover:opacity-70
+    max-md:py-4 max-md:px-8"
+    >
       Get Started
     </button>
   );
