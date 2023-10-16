c4dff318438bfc6c3862879062082f5b67a695d1
Chris Gibbons <christ487@icloud.com>
2023-10-16 19:16:38 +0100

    add css style for mobile

--- manage-landing-page/src/heroNavSection.js
+++ manage-landing-page/src/heroNavSection.js
@@ -2,13 +2,14 @@ import React from "react";
 import HeroButton from "./components/heroButton";
 import Logo from "./images/logo.svg";
 import HeroImage from "./images/illustration-intro.svg";
+import Hamburger from "./images/icon-hamburger.svg";
 
 function heroNavSection() {
   return (
     <div className="box-border font-BeVietnamPro">
       {/* navigation Bar */}
 
-      <nav className="flex justify-between items-center pt-6 pb-14 px-28">
+      <nav className="flex justify-between items-center pt-10 pb-14 px-28 max-lg:hidden">
         <span>
           <img src={Logo} alt="MainLogo" className="hover:cursor-cell"></img>
         </span>
@@ -23,20 +24,36 @@ function heroNavSection() {
         </div>
         <HeroButton />
       </nav>
+
+      {/* mobile menud */}
+
+      <div className="flex justify-between items-center px-14 pt-10 lg:hidden">
+        <span>
+          <img src={Logo} alt="MainLogo" className="hover:cursor-cell"></img>
+        </span>
+        <span>
+          <img
+            src={Hamburger}
+            alt="Hamburger"
+            className="hover:cursor-cell"
+          ></img>
+        </span>
+      </div>
+
       {/* Hero Section */}
-      <div className="flex px-28 h-screen ">
-        <div className="flex justify-center ">
-          <div className=" flex-col items-center  w-1/2 pt-20">
-            <h1 className="text-5xl font-bold darkBlue pb-10">
+      <div className="px-28 max-lg:px-6 max-lg:h-screen">
+        <div className="flex justify-center max-lg:flex-col-reverse pt-10">
+          <div className="flex-col items-center  w-1/2 pt-20 max-lg:w-full max-lg:pt-0 max-lg:text-center">
+            <h1 className="text-5xl font-bold darkBlue pb-10 max-lg:pt-6">
               Bring everyone together to build better products.
             </h1>
-            <p className="text-sm greyFont pb-10 w-2/3 pr-4">
+            <p className="text-sm greyFont pb-10 w-2/3 pr-4 max-lg:w-full max-lg:text-lg">
               Manage makes it simple for software teams to plan day-to-day tasks
               while keeping the larger team goals in view.
             </p>
             <HeroButton />
           </div>
-          <div className="flex items-center pl-20 pb-20">
+          <div className="flex items-center justify-center pl-20 pb-20 max-lg:p-0">
             <img src={HeroImage} alt="MainLogo" className=""></img>
           </div>
         </div>
