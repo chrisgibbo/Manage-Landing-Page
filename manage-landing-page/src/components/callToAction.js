2ba627d8c189ae2338eda85ca085ffe12a1cbf99
Chris Gibbons <christ487@icloud.com>
2023-10-16 20:31:16 +0100

    adjust css for mobile size

--- manage-landing-page/src/components/callToAction.js
+++ manage-landing-page/src/components/callToAction.js
@@ -3,9 +3,12 @@ import "../App.css";
 
 function callToAction() {
   return (
-    <div className="px-28 flex justify-between items-center h-44 brightRedBkGround font-BeVietnamPro">
-      <div className="w-2/5">
-        <h2 className="text-white font-medium text-3xl ">
+    <div
+      className="px-28 py-10 flex justify-between items-center brightRedBkGround font-BeVietnamPro
+     max-md:flex-col max-md:px-6 max-md:text-center max-md:py-24"
+    >
+      <div className="w-2/5 max-lg:w-full max-md:mb-10">
+        <h2 className="text-white font-medium text-3xl max-md:text-4xl max-md:px-14">
           Simplify how your team works today.
         </h2>
       </div>
