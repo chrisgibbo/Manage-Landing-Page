f7978f6d87ebd1b00890738a0538acec8dcf76a2
Chris Gibbons <christ487@icloud.com>
2023-10-16 19:35:14 +0100

    add css style for mobile

--- manage-landing-page/src/components/infoSection.js
+++ manage-landing-page/src/components/infoSection.js
@@ -3,13 +3,23 @@ import "../App.css";
 
 const infoSection = (props) => {
   return (
-    <div className="flex items-start pl-20 pb-10 gap-8 font-BeVietnamPro">
-      <button className="brightRed py-2 px-5 font-bold text-white text-xs rounded-full">
+    <div className="flex items-start pl-20 pb-10 gap-8 font-BeVietnamPro max-lg:pl-0 max-lg:gap-2">
+      <button
+        className="brightRed py-2 px-5 font-bold text-white text-xs rounded-full
+      max-lg:py-3 max-lg:px-8 max-lg:text-lg"
+      >
         {props.button}
       </button>
       <div>
-        <h3 className="darkBlue font-bold text-sm pb-4">{props.title}</h3>
-        <p className="greyFont text-sm">{props.paragraph}</p>
+        <h3
+          className="darkBlue font-bold text-sm pb-4 
+        max-lg:text-lg max-lg:bg-red-100 max-lg:p-3 max-lg:pl-2 max-lg:mb-4 max-lg:rounded-xl"
+        >
+          {props.title}
+        </h3>
+        <p className="greyFont text-sm max-lg:text-base max-lg:pr-4">
+          {props.paragraph}
+        </p>
       </div>
     </div>
   );
