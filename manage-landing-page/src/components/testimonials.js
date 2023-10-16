f4958f8b1b4243cc79f4a802bbc8c8e6cfa946b8
Chris Gibbons <christ487@icloud.com>
2023-10-16 19:52:01 +0100

    add css style mobile to testimonial card

--- manage-landing-page/src/components/testimonials.js
+++ manage-landing-page/src/components/testimonials.js
@@ -3,17 +3,19 @@ import "../App.css";
 
 function testimonials(props) {
   return (
-    <div className="flex flex-col items-center mx-2 mt-10 mb-6 greyBackground relative h-44 hover:cursor-move">
+    <div className="flex flex-col items-center mx-2 mt-10 mb-6 greyBackground relative h-44 hover:cursor-move max-md:h-72">
       <div className="flex flex-col items-center h-24 ">
         <img
           src={props.image}
           alt="Persons Face"
-          className="h-1/3 absolute top-[-28px]"
+          className="h-1/3 absolute top-[-28px] max-md:top-[-38px]"
         ></img>
       </div>
-      <div className="pb-7 px-6">
-        <h3 className="darkBlue text-sm font-bold pb-6">{props.name}</h3>
-        <p className="greyFont text-xs">{props.testimonial}</p>
+      <div className="pb-7 px-6 max-md:p-0">
+        <h3 className="darkBlue text-sm font-bold pb-6 max-md:text-xl max-lg:text-bold">
+          {props.name}
+        </h3>
+        <p className="greyFont text-xs max-md:text-lg">{props.testimonial}</p>
       </div>
     </div>
   );
