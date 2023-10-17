6bfd171e5b6dbb10f53b27475614026781f72da4
Chris Gibbons <christ487@icloud.com>
2023-10-17 20:40:47 +0100

    change button to span

--- manage-landing-page/src/components/infoSection.js
+++ manage-landing-page/src/components/infoSection.js
@@ -4,12 +4,12 @@ import "../App.css";
 const infoSection = (props) => {
   return (
     <div className="flex items-start pl-20 pb-10 gap-8 font-BeVietnamPro max-lg:pl-0 max-lg:gap-2">
-      <button
+      <span
         className="brightRed py-2 px-5 font-bold text-white text-xs rounded-full
       max-lg:py-3 max-lg:px-8 max-lg:text-lg transition-transform transform hover:scale-105"
       >
         {props.button}
-      </button>
+      </span>
       <div>
         <h3
           className="darkBlue font-bold text-sm pb-4 
