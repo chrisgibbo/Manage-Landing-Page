cbe0e938a4bdba393ff528f5f6d1c3b38fd91301
Chris Gibbons <christ487@icloud.com>
2023-10-17 20:36:17 +0100

    add transition to css on hover

--- manage-landing-page/src/components/heroButton.js
+++ manage-landing-page/src/components/heroButton.js
@@ -5,7 +5,7 @@ function HeroButton() {
   return (
     <button
       className="HeroBtn py-3 px-7 text-xs rounded-full font-semibold font-BeVietnamPro hover:opacity-70
-    max-md:py-4 max-md:px-8"
+    max-md:py-4 max-md:px-8 transition-transform transform hover:scale-105"
     >
       Get Started
     </button>
