85ae85bfda5fe1e729fe6c1262e609915c2b21f1
Chris Gibbons <christ487@icloud.com>
2023-10-17 19:31:58 +0100

    style footer for mobile screen

--- manage-landing-page/src/footer.js
+++ manage-landing-page/src/footer.js
@@ -15,43 +15,52 @@ const handleMouseOut = ({ target }) => (target.style.color = "#fff");
 
 function footer() {
   return (
-    <div className="px-28 py-12 flex justify-between font-BeVietnamPro footerBackground">
+    <div className="px-28 py-12 flex justify-between font-BeVietnamPro footerBackground max-md:flex-col-reverse max-md:px-6">
       {/* Logo + social icons div */}
 
-      <div className="flex flex-col justify-between items-center">
+      <div className="flex flex-col justify-between items-center max-md:flex-col-reverse">
         <div>
-          <img src={LogoWhite} alt="MainLogoWhite"></img>
+          <p className="greyFont text-xs md:hidden max-md:text-base">
+            Copyright 2023 All Rights Reserved.
+          </p>
         </div>
-        <div className="flex gap-3">
-          <a href="*">
+        <div>
+          <img
+            src={LogoWhite}
+            alt="MainLogoWhite"
+            className="max-md:my-14 max-md:h-8"
+          ></img>
+        </div>
+        <div className="flex gap-3 max-md:mt-10 max-md:gap-8">
+          <a href="*" className="max-md:text-3xl">
             <FaFacebookSquare
               style={style}
               onMouseOver={handleMouseOver}
               onMouseOut={handleMouseOut}
             />
           </a>
-          <a href="*">
+          <a href="*" className="max-md:text-3xl">
             <FaYoutube
               style={style}
               onMouseOver={handleMouseOver}
               onMouseOut={handleMouseOut}
             />
           </a>
-          <a href="*">
+          <a href="*" className="max-md:text-3xl">
             <FaTwitter
               style={style}
               onMouseOver={handleMouseOver}
               onMouseOut={handleMouseOut}
             />
           </a>
-          <a href="*">
+          <a href="*" className="max-md:text-3xl">
             <FaPinterest
               style={style}
               onMouseOver={handleMouseOver}
               onMouseOut={handleMouseOut}
             />
           </a>
-          <a href="*">
+          <a href="*" className="max-md:text-3xl">
             <FaInstagram
               style={style}
               onMouseOver={handleMouseOver}
@@ -63,7 +72,7 @@ function footer() {
 
       {/* footer navlinks div */}
 
-      <div className="flex justify-between gap-44 footerfont text-sm leading-8">
+      <div className="flex justify-between gap-44 footerfont text-sm leading-8 max-md:justify-center max-md:gap-32 max-md:leading-10 max-md:text-base">
         <div>
           <ul>
             <li className="hover:text-red-400">
@@ -98,17 +107,19 @@ function footer() {
       {/* newsletter signup */}
 
       <div className="flex flex-col justify-between items-center">
-        <form className="">
+        <form className="max-md:flex max-md:mb-10">
           <input
             type="email"
             placeholder="Updates in your inbox ..."
-            className="rounded-full px-8 py-3 text-xs outline-none"
+            className="rounded-full px-8 py-3 text-xs outline-none max-md:w-64"
           />
-          <button className="brightRedBkGround rounded-full px-5 py-3 text-xs font-semibold footerfont ml-2 hover:brightness-150">
+          <button className="brightRedBkGround rounded-full px-5 py-3 text-xs font-semibold footerfont ml-2 hover:brightness-150 max-md:px-8 max-md:py-4">
             Go
           </button>
         </form>
-        <p className="greyFont text-xs">Copyright 2023 All Rights Reserved.</p>
+        <p className="greyFont text-xs max-md:hidden">
+          Copyright 2023 All Rights Reserved.
+        </p>
       </div>
     </div>
   );
